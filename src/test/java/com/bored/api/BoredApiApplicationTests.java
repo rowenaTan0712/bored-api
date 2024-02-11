ff7c58d9c7dded46781e8b7f7a9848800a474adf
Rowena Tan <rowena.tan@dtn.com>
2024-02-11 21:49:12 +0800

    initial project commit

---
+++ src/test/java/com/bored/api/BoredApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bored.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
